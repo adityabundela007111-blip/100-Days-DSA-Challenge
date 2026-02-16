bd8d7c3403da2f584ab9c2134fc00f142d13d6ac
Aditya Singh Bundela <adityabundela007111@gmail.com>
2026-02-16 16:40:23 +0530

    Implement array insertion in C

--- Day01_A.c
+++ Day01_A.c
@@ -2,20 +2,26 @@
 
 int main() {
     int n, pos, x;
+    int arr[100];
+    int i;
 
     scanf("%d", &n);
-    int arr[n + 1];
-    for(int i = 0; i < n; i++) {
+
+    for(i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
     scanf("%d", &pos);
+
     scanf("%d", &x);
-    for(int i = n; i >= pos; i--) {
+
+    for(i = n; i >= pos; i--) {
         arr[i] = arr[i - 1];
     }
+
     arr[pos - 1] = x;
-    for(int i = 0; i <= n; i++) {
+
+    for(i = 0; i <= n; i++) {
         printf("%d ", arr[i]);
     }
 
